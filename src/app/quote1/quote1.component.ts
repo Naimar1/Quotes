adbefe3690df1175fe29a49040b501e548a64c74
Naimar1 <naiglyme1@gmail.com>
2019-01-30 10:09:19 +0200

    Add name of the author

--- src/app/quote1/quote1.component.ts
+++ src/app/quote1/quote1.component.ts
@@ -12,7 +12,7 @@ export class Quote1Component implements OnInit {
   newQuote =new Quoteclass(0,"","",new Date(),0,0);
   quotes = [
     new Quoteclass(1,'Bonhoeffer','In ordinary life we hardly realize that we receive a great deal more than we give, and that it is only with gratitude that life becomes rich.',new Date(2019,1,20),0,0),
-    new Quoteclass(2,'Quote-thanks','Saying thank you is more than good manners, it is good spirituality',new Date(2019,1,23),0,0),
+    new Quoteclass(2,'Painter','Saying thank you is more than good manners, it is good spirituality',new Date(2019,1,23),0,0),
     new Quoteclass(3,'C.S. Lewis','Friendship is unnecessary, like philosophy, like art…. It has no survival value; rather it is one of those things that give value to survival.',new Date(2019,1,24),0,0),
     new Quoteclass(4,'Calcutta','If we have no peace, it is because we have forgotten that we belong to each other.',new Date(2019,1,25),0,0),
 
