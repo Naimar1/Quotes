76f6a1c09aafe5b378319313538e510e74d30490
Naimar1 <naiglyme1@gmail.com>
2019-01-30 10:37:25 +0200

    Add name for date

--- src/app/quoteclass.ts
+++ src/app/quoteclass.ts
@@ -1,6 +1,6 @@
 export class Quoteclass {
     public showImagequote:boolean;
-    constructor(public id:number, public name:string,public image:string, public newd:Date, public upvote:number,public downvote:number){
+    constructor(public id:number, public name:string,public image:string, public completeDate:Date, public upvote:number,public downvote:number){
         this.showImagequote=false;
 
     }
